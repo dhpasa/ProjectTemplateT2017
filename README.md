5c414cd98a62c5b046e7cbb656be88d13581caf9
linliuan <linliuanqdzb@163.com>
2017-03-31 21:36:09 +0800

    添加多数据源

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # tech-coffee 框架模板
 
-##技术栈
+## 技术栈
 
 `springmvc` `mybatis` .......
 
-##重构内容
-###spring mybatis 改用最新的release版本包。
-###工程全局异常的配置，取消controller捕获异常，可实现异常的统一处理。
-###废弃log4j改用logback进行log管理。可以实现日志的扩展
-###web.xml中配置sql语句监控画面 druid/sql.html。（可观察sql执行耗时）
-###获取工程配置信息优化。所有调用的地方都用BaseUtils 下的方法。
-###监控指定controller时长，超过警告时间则输出log。
+## 重构内容
+### spring mybatis 改用最新的release版本包。
+### 工程全局异常的配置，取消controller捕获异常，可实现异常的统一处理。
+### 废弃log4j改用logback进行log管理。可以实现日志的扩展
+### web.xml中配置sql语句监控画面 druid/sql.html。（可观察sql执行耗时）
+### 获取工程配置信息优化。所有调用的地方都用BaseUtils 下的方法。
+### 监控指定controller时长，超过警告时间则输出log。
+### 实现多数据源操作，例如数据库读写分离后的操作。
 
---
+++ project_core/src/main/java/com/org/techcoffee/base/dao/BaseSlaveDao.java
@@ -0,0 +1,217 @@
+package com.org.techcoffee.base.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.ibatis.session.RowBounds;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.dao.DataAccessException;
+
+import com.org.techcoffee.base.entity.BaseEntity;
+import com.org.techcoffee.base.page.Pagination;
+import com.org.techcoffee.base.page.PagingResult;
+
+/**
+ * @ClassName: BaseDao
+ * @Description: 这里需要在spring-config里面配置数据元，有个数据源的操作。比如数据库读写分离
+ */
+public abstract class BaseSlaveDao extends SqlSessionDaoSupport {
+
+    @Resource(name = "sqlSlaveSession")
+    public SqlSessionTemplate sqlSession;
+
+    @Override
+    @Resource(name = "sqlSlaveSession")
+    public void setSqlSessionTemplate(SqlSessionTemplate sqlSession) {
+        super.setSqlSessionTemplate(sqlSession);
+    }
+
+    public Integer insert(String statementName, Object entity) {
+        return sqlSession.insert(statementName, entity);
+    }
+
+    public Integer insertForBatch(String statementName, Object entity) {
+        return sqlSession.insert(statementName, entity);
+    }
+
+    public Integer update(String statementName, Object entity) {
+        return sqlSession.update(statementName, entity);
+    }
+
+    public Integer insertForBbs(String statementName, Object entity) {
+        return sqlSession.insert(statementName, entity);
+    }
+    
+    
+
+    /**
+     * 更新考虑排他
+     * 
+     * @param statementName
+     * @param entity
+     * @return
+     */
+    public Integer updateForConcurrent(String statementName, BaseEntity entity) {
+        int updateCount = sqlSession.update(statementName, entity);
+        if (updateCount == 0) {
+            //throw new DBConcurrencyException(MessageUtils.getText("err.com.db.concurrent"));
+        	//TODO
+        	return 0;
+        }
+        else {
+            return updateCount;
+        }
+    }
+
+    public int delete(String statementName, BaseEntity entity) {
+        return sqlSession.delete(statementName, entity);
+    }
+    
+    public int deleteObj(String statementName, Object entity) {
+        return sqlSession.delete(statementName, entity);
+    }
+
+    /**
+     * 删除考虑并发
+     * 
+     * @param statementName
+     * @param entity
+     * @return
+     */
+    public int deleteForConcurrent(String statementName, BaseEntity entity) {
+        int updateCount = sqlSession.delete(statementName, entity);
+        if (updateCount == 0) {
+            //throw new DBConcurrencyException(MessageUtils.getText("err.com.db.concurrent"));TODO
+        	return 0;
+        }
+        else {
+            return updateCount;
+        }
+    }
+
+    public <T> PagingResult<T> selectPagination(String statementName, String countStatementName, Pagination pagination)
+            throws RuntimeException {
+        try {
+            // 默认为第一页
+            int page = pagination.getPage();
+            // 默认每页15个
+            int size = pagination.getSize();
+            // 页码默认10
+            int pageNoSize = pagination.getPageNoSize();
+
+            RowBounds rowBounds = new RowBounds((page - 1) * size, size);
+
+            Map<Object, Object> param = pagination.getParams();
+            if (param != null) {
+                param.put("orderColumn", pagination.getOrderColumn());
+                param.put("orderTurn", pagination.getOrderTurn());
+            }
+            else {
+                param = new HashMap<Object, Object>();
+                param.put("orderColumn", pagination.getOrderColumn());
+                param.put("orderTurn", pagination.getOrderTurn());
+            }
+
+            List<T> resultList = sqlSession.selectList(statementName, param, rowBounds);
+            int totalSize = count(countStatementName, pagination.getParams());
+
+            PagingResult<T> pagingResult = new PagingResult<T>();
+            pagingResult.setCurrentPage(page);
+            pagingResult.setTotalSize(totalSize);
+            pagingResult.setPageSize(size);
+            pagingResult.setResultList(resultList);
+            pagingResult.setPageNoSize(pageNoSize);
+            return pagingResult;
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> PagingResult<T> selectPagination(String statementName, Pagination pagination) throws RuntimeException {
+        try {
+            // 默认为第一页
+            int page = pagination.getPage();
+            // 默认每页15个
+            int size = pagination.getSize();
+            //页码默认10
+            int pageNoSize = pagination.getPageNoSize();
+
+            RowBounds rowBounds = new RowBounds((page - 1) * size, size);
+
+            Map<Object, Object> param = pagination.getParams();
+            if (param != null) {
+                param.put("orderColumn", pagination.getOrderColumn());
+                param.put("orderTurn", pagination.getOrderTurn());
+            }
+            else {
+                param = new HashMap<Object, Object>();
+                param.put("orderColumn", pagination.getOrderColumn());
+                param.put("orderTurn", pagination.getOrderTurn());
+            }
+
+            List<T> resultList = sqlSession.selectList(statementName, param, rowBounds);
+            int totalSize = select(statementName, param).size();
+
+            PagingResult<T> pagingResult = new PagingResult<T>();
+            pagingResult.setCurrentPage(page);
+            pagingResult.setTotalSize(totalSize);
+            pagingResult.setPageSize(size);
+            pagingResult.setResultList(resultList);
+            pagingResult.setPageNoSize(pageNoSize);
+            return pagingResult;
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T selectOne(String statementName, Object obj) throws RuntimeException {
+        try {
+            return sqlSession.selectOne(statementName, obj);
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int count(String statementName, Object param) throws RuntimeException {
+        try {
+            return sqlSession.selectOne(statementName, param);
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> select(String statementName) throws RuntimeException {
+        try {
+            return sqlSession.selectList(statementName);
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> select(String statementName, Object obj) throws RuntimeException {
+        try {
+            return sqlSession.selectList(statementName, obj);
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void executeSP(String statementName, Object param) {
+        try {
+            sqlSession.selectOne(statementName, param);
+        }
+        catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
